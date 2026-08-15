ef18f2d7f9de39ab175d7baf11dfe78e545343f6
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-08-15 11:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.89%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -1,5 +1,5 @@
 class Solution(object):
     def convertTemperature(self, celsius):
-        Kelvin = celsius + 273.15
-        Fahrenheit = celsius * 1.80 + 32.00
-        return [Kelvin, Fahrenheit]
+        K = celsius + 273.15
+        F = celsius * 1.80 + 32.00
+        return [K, F]
