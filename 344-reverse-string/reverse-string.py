e1c7f00d22e558cba25937d998d53b6745df9617
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-08-18 20:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (43.19%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,8 +3,6 @@ class Solution(object):
         left = 0
         right = len(s)-1
         while left <= right:
-            temp = s[left]
-            s[left] = s[right]
-            s[right] = temp
+            s[left], s[right] =s[right], s[left]
             left += 1
             right -= 1
