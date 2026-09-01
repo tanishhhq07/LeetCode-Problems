8294cefbc72bdf63b81f95c8c8a2fef0679e1bf1
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-01 20:41:02 +0530

    Time: 24 ms (42.86%) | Memory: 12.5 MB (14.29%) - LeetSync

--- 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.py
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.py
@@ -2,25 +2,20 @@ class Solution(object):
     def isWinner(self, player1, player2):
         score1 = 0
         score2 = 0
+
         for i in range(len(player1)):
-            if i > 0 and player1[i-1] == 10:
-                score1 += player1[i] * 2
-            elif i > 1 and player1[i-2] == 10:
+            if (i > 0 and player1[i-1] == 10) or (i > 1 and player1[i-2] == 10):
                 score1 += player1[i] * 2
             else:
                 score1 += player1[i]
 
-        for i in range(len(player2)):
-            if i > 0 and player2[i-1] == 10:
-                score2 += player2[i] * 2
-            elif i > 1 and player2[i-2] == 10:
+            if (i > 0 and player2[i-1] == 10) or (i > 1 and player2[i-2] == 10):
                 score2 += player2[i] * 2
             else:
                 score2 += player2[i]
 
         if score1 > score2:
             return 1
-        elif score1 < score2:
+        if score2 > score1:
             return 2
-        else:
-            return 0
+        return 0
