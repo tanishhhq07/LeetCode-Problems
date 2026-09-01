ebcb76cd9f4498c5a6da13248e03b0bf0fd02f56
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-01 19:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (44.51%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def calPoints(self, operations):
         arr = []
-        score = 0
         for i in range(len(operations)):
             if operations[i].lstrip("-").isdigit():
                 arr.append(int(operations[i]))
@@ -11,8 +10,6 @@ class Solution(object):
                 arr.append(arr[-1] * 2)
             elif operations[i] == "+":
                 arr.append(arr[-1] + arr[-2])
-        for i in arr:
-            score += i
-        return score
+        return sum(arr)
 
         
