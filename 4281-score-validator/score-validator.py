49b72d263d880bad8d88450ca4cbc05ca906a6de
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-01 15:46:16 +0530

    Time: 5 ms (62.84%) | Memory: 12.4 MB (96.62%) - LeetSync

--- 4281-score-validator/score-validator.py
+++ 4281-score-validator/score-validator.py
@@ -1,18 +1,17 @@
 class Solution(object):
     def scoreValidator(self, events):
-        total = 0
+        score = 0
         counter = 0
-        digits = "012346"
         for i in events:
-            if i in digits:
-                total = total + int(i)
+            if i in "012346":
+                score = score + int(i)
             elif i == "WD" or i == "NB":
-                total += 1
+                score += 1
             elif i == "W":
                 counter += 1
             if counter == 10:
                 break
-        arr = []
-        arr.append(total)
-        arr.append(counter)
-        return arr
+        a = []
+        a.append(score)
+        a.append(counter)
+        return a
