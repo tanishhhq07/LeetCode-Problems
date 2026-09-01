b8585b2a087786b60fa0c3ce81003b511539b4a8
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-01 22:57:58 +0530

    Time: 3 ms (56.82%) | Memory: 12.4 MB (57.39%) - LeetSync

--- 4178-vowel-consonant-score/vowel-consonant-score.py
+++ 4178-vowel-consonant-score/vowel-consonant-score.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def vowelConsonantScore(self, s):
         vowels = "aeiou"
-        cons = "bcdfghjklmnpqrstvwxyz"
         v = 0
         c = 0
         for i in s:
-            if i in cons:
-                c += 1
-            elif i in vowels:
-                v += 1
+            if i.isalpha():
+                if i in vowels:
+                    v += 1
+                else:
+                    c += 1
 
         if c == 0:
             return 0
