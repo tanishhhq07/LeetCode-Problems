7d2591b0da30d198fee725af47f67e7f0db15529
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-05 20:27:52 +0530

    Time: 14 ms (65.20%) | Memory: 12.3 MB (56.15%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def sortSentence(self, s):
         arr = [""] * len(s.split())
-        num = 0
         for i in s.split():
-            for j in i:
-                if j.isdigit():
-                    num = int(j)
-                    arr[num -1] = i[:-1]
+            num = int(i[-1])
+            arr[num-1] = i[:-1]
         return " ".join(arr) 
