201f1bbf0d0c01abe56e821992796c6d06a17005
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-07 22:54:26 +0530

    Time: 13 ms (87.95%) | Memory: 14.2 MB (40.47%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,8 +1,3 @@
 class Solution(object):
     def distributeCandies(self, candyType):
-        n = len(candyType)
-        types = len(set(candyType))
-
-        if types < n / 2:
-            return types
-        return n / 2
+        return min(len(set(candyType)) , len(candyType) // 2)
