08820256b8785ee2eebd33d8dbd8b581c4d765cb
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-21 21:41:13 +0530

    Time: 3 ms (37.76%) | Memory: 12.4 MB (19.39%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def freqAlphabets(self, s):
-        numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9","10#", "11#", "12#", "13#", "14#", "15#", "16#","17#", "18#", "19#", "20#", "21#", "22#", "23#","24#", "25#", "26#"]
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-        ans = ""
+        ans = []
         i = 0
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
         while i < len(s):
-            if i + 2 < len(s) and s[i + 2] == "#":
-                code = s[i:i + 3]
+            if i + 2 < len(s) and s[i + 2] == '#':
+                num = int(s[i:i+2])
                 i += 3
             else:
-                code = s[i]
+                num = int(s[i])
                 i += 1
-            ans += alphabet[numbers.index(code)]
-        return ans
+
+            ans.append(alphabet[num - 1])
+
+        return ''.join(ans)
