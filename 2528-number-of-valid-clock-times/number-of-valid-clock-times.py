1f767c15dfc546ab4e6c8abc9b12e9629e220b22
Tanishq Mhatre <tanishqmhatre1317@gmail.com>
2026-09-26 16:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (47.37%) - LeetSync

--- 2528-number-of-valid-clock-times/number-of-valid-clock-times.py
+++ 2528-number-of-valid-clock-times/number-of-valid-clock-times.py
@@ -1,15 +1,32 @@
 class Solution(object):
     def countTime(self, time):
-        count = 0
-        for hour in range(24):
-            for minute in range(60):
-                current = "%02d:%02d" % (hour, minute)
-                valid = True
-                for i in range(5):
-                    if time[i] != "?" and time[i] != current[i]:
-                        valid = False
-                        break
-                if valid:
-                    count += 1
+        h1 = time[0]
+        h2 = time[1]
+        m1 = time[3]
+        m2 = time[4]
 
-        return count
+        if h1 == "?" and h2 == "?":
+            hours = 24
+        elif h1 == "?":
+            if h2 <= "3":
+                hours = 3
+            else:
+                hours = 2
+        elif h2 == "?":
+            if h1 == "2":
+                hours = 4
+            else:
+                hours = 10
+        else:
+            hours = 1
+
+        if m1 == "?" and m2 == "?":
+            minutes = 60
+        elif m1 == "?":
+            minutes = 6
+        elif m2 == "?":
+            minutes = 10
+        else:
+            minutes = 1
+
+        return hours * minutes
